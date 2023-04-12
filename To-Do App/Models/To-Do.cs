efc39a31aeec0cf79f544d2ffb345d326b9a74b9
johnnyfad <johnlejudge@gmail.com>
2023-04-12 18:03:24 -0400

    data added to DBContext for modelling

--- To-Do App/Models/To-Do.cs
+++ To-Do App/Models/To-Do.cs
@@ -1,6 +1,43 @@
-﻿namespace To_Do_App.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace To_Do_App.Models
 {
-    public class To_Do
+    public class Todo
     {
+        [Key]
+        public string Title { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+
+
+        public List<Todo> getTodoList()
+        {
+            List<Todo> todos = new List<Todo>();
+            todos.Add(new Todo { Title = "Play basketball", IsCompleted = true });
+            todos.Add(new Todo { Title = "Shopping", IsCompleted = false });
+            todos.Add(new Todo { Title = "Assignment", IsCompleted = true });
+            todos.Add(new Todo { Title = "Workout", IsCompleted = false });
+            todos.Add(new Todo { Title = "Make food", IsCompleted = false });
+            todos.Add(new Todo { Title = "Cleaning", IsCompleted = true });
+
+            return todos;
+        }
+
+
+        public List<Todo> getIsnotCompletedList()
+        {
+
+            List<Todo> isNotCompletedList = new List<Todo>();
+            foreach (Todo todo in new Todo().getTodoList())
+            {
+                if (todo.IsCompleted == false)
+                {
+                    isNotCompletedList.Add(todo);
+                }
+            }
+            return isNotCompletedList;
+        }
+
     }
 }
